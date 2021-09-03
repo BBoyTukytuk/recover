41d6e1cd8919456b99139703de501ee851223866
BBoyTukytuk <63485300+BBoyTukytuk@users.noreply.github.com>
2021-09-03 10:03:16 +1000

    Update recover.c

--- recover.c
+++ recover.c
@@ -10,7 +10,7 @@ if not first Jpeg, close previous Jpeg
 number the files it outputs by naming each ###.jpg
 int sprintf(char *str, const char *format, [arg1, arg2, ... ]);
 int main() {
-    char output[50]; 
+    char output[8; 
     int num1 = 3, num2 = 5, ans; 
     ans = num1 * num2; 
     sprintf(output, "%d multiplied by %d is %d", num1, num2, ans);  
@@ -52,12 +52,12 @@ int main(int argc, char *argv[]) // This creates a pointer for the address of th
     BYTE buffer [block_size]; //create an ARRAY of 512 bytes to store the data from the card input to a buffer / header.
     // uint8_t arrayName arraySize or 512 = HEADER_SIZE
 
-    FILE *image = NULL; // creates a pointer to a file image that will store all the image data. We will have to close this
+    FILE *image = NULL; // creates a pointer to a file image that will store all the image data. We will have to close this soemthing we will use in the future
 
     int jpeg_count = 0; //Count must start at 000
-    char file_name[20]; // how long the name of the file will be
+    char file_name[8]; // how long the name of the array we will put the name in
     
-    while ((fread(&buffer, sizeof(BYTE) , block_size , input)) == block_size) // creates a ptr to buffer of 512 bytes and keeps reading till no more blocks
+    while ((fread(buffer, sizeof(block_size) , 1 , input)) == 1) // fread will return a number 1 or 0 that is true or false
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
@@ -65,17 +65,17 @@ int main(int argc, char *argv[]) // This creates a pointer for the address of th
             if (jpeg_count != 0) // if it does not equal 0
             {
                 fclose(image); // then I wilclose
-                sprintf(file_name, "%03i.jpg", jpeg_count); //%03i means print an "i" integer with "3" digits to represent it
             }
-            else 
-            {
-                sprintf(file_name, "%03i.jpg", jpeg_count); //%03i means print an integer with 3 digits to represent it
-  
-            }
-            jpeg_count +=1; 
+            
+            sprintf(file_name, "%03i.jpg", jpeg_count++); //%03i means print an integer with 3 digits to represent it ++ can count 
+            image = fopen(file_name, "w"); // Open file called image with the jpeg name
+            fwrite(buffer, block_size , 1, image);
         }
-        image = fopen(file_name, "w"); // Open file called image with the jpeg name
-        fwrite(&buffer, block_size , 1, image);
+        else if (jpeg_count > 0)
+        {
+            fwrite(buffer, block_size , 1, image);
+
+        }
     }
 
     fclose(input);
